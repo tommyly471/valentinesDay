845df118053302b1d78dbe15551e4f776dfad568
tommyly471 <tommyly471@gmail.com>
2024-02-13 20:35:27 -0600

    starting

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const phrases = [
+  "no",
+  "Are you sure?",
+  "Really sure?",
+  "Pookie please",
+  "Don't do this to me",
+  "I'm gonna cry...",
+  "You're breaking my heart",
+  "Yes is your only answer now",
+];
+
+function App() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  function handleNoClick() {
+    setNoCount(noCount + 1);
+  }
+
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
+
+  return (
+    <div className="valentine-container">
+      {yesPressed ? (
+        <>
+          <img
+            alt="bears kissing"
+            src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbzR6M3hkNGRuZ2Zyc2E0Y2xpZ2lsbXFhdzh5NXlpNGk4dnhvbGhrNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1JmGiBtqTuehfYxuy9/giphy.gif"
+          />
+          <div className="text">YAY!!!!!</div>
+        </>
+      ) : (
+        <>
+          <img
+            alt="bear with hearts"
+            src="https://media.giphy.com/media/qFmdpUKAFZ6rMobzzu/giphy.gif"
+          />
+          <div> Will you be my valentine?</div>
+          <div>
+            <button
+              className="yesButton"
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button onClick={handleNoClick} className="noButton">
+              {getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
